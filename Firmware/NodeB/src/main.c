fdc11fa94f5c9c2bd62e0fe80a28564cdd28f0d9
YamnSSalim <144996642+YamnSSalim@users.noreply.github.com>
2025-12-16 01:53:12 +0100

    Parse manufacturer data from ESP32 Node A advertisements

--- Firmware/NodeB/src/main.c
+++ Firmware/NodeB/src/main.c
@@ -4,26 +4,69 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/bluetooth/hci.h>
 
+#include <zephyr/sys/byteorder.h>
 
-LOG_MODULE_REGISTER(rtt_test, LOG_LEVEL_INF);
+
+LOG_MODULE_REGISTER(node_b, LOG_LEVEL_INF);
+
+// Config 
+#define COMPANY_ID 0xFFFF
+#define NODE_ID_A 1 
+
+
+
+// Manufacturer parser 
+static bool ad_parse_cb(struct bt_data *data, void *user_data)
+{
+        /* data */
+        ARG_UNUSED(user_data);
+
+        if (data->type != BT_DATA_MANUFACTURER_DATA){
+                return true; // continue parsing
+        }
+
+        if (data->data_len < 6) {
+                LOG_WRN("Manufacturer data too short (%u)", data->data_len);
+                return false; 
+        }
+
+        const uint8_t *d = data->data; 
+
+        uint16_t company_id = sys_get_le16(&d[0]); 
+        uint8_t node_id = d[2];
+        uint8_t seq = d[3]; 
+        int16_t temp_raw = sys_get_le16(&d[4]); 
+
+        if (company_id !=  COMPANY_ID || node_id != NODE_ID_A){
+                return false; 
+        }
+
+        LOG_INF("Node A | seq=%u | temp_raw=%d (%.2f C)",
+                seq,
+                temp_raw,
+                temp_raw / 100.0f);
+
+        return false; // Stop parsing this packet
+}
+
+
 
 // Callback: Advertising packet is recieved
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type, struct net_buf_simple *ad)
+static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type, struct net_buf_simple *buf)
 {
-        char addr_str[BT_ADDR_LE_STR_LEN];
+        ARG_UNUSED(addr);
+        ARG_UNUSED(rssi);
+        ARG_UNUSED(adv_type); 
 
-        bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
-
-        LOG_INF("Device found: %s | RSSI %d | AD len %u", addr_str, rssi, ad->len);
-};
+        bt_data_parse(buf, ad_parse_cb, NULL);
+}
                         
 
 int main(void)
 {
         int err; 
 
-        printk("Node B: RTT + BLE scan starting\n"); 
-        LOG_INF("RTT logging works"); 
+        LOG_INF("Node B: Starting BLE scanner"); 
 
         // Enable Bluetooth
         err = bt_enable(NULL); 
@@ -32,24 +75,28 @@ int main(void)
                 return 0;
         }
 
+        LOG_INF("Bluetooth initialized");
+
+
         // Scan parameters
         struct bt_le_scan_param scan_param =
         {
                 /* data */
                 .type = BT_LE_SCAN_TYPE_PASSIVE,
                 .options = BT_LE_SCAN_OPT_NONE,
-                .interval = BT_GAP_SCAN_FAST_WINDOW,
+                .interval = BT_GAP_SCAN_FAST_INTERVAL,
                 .window = BT_GAP_SCAN_FAST_WINDOW,
         };
 
+
         // Start scanning
-        err = bt_le_scan_start(&scan_param, device_found); 
+        err = bt_le_scan_start(&scan_param, scan_cb); 
         if (err){ 
                 LOG_ERR("Scanning failed to start (err %d)", err);
                 return 0;
         }
 
-        LOG_INF("Scanning started");
+        LOG_INF("Scanning for ESP32 Node A");
 
         // No main loop everything happens in callback
 
